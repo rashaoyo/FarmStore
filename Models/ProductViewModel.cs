7a11786bb7c8f872b32a8613463b733009e93c2a
Vlad <42613754+vladi3ir@users.noreply.github.com>
2019-10-30 18:31:44 -0400

    Added a product view moded

---
+++ Models/ProductViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace FarmStore.Models
+{
+    public class ProductViewModel
+    {
+        public int ID { get; set; }
+        public string ProductName { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+    }
+}
